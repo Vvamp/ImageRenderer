c3e53f6ce6cdf7f59ba708db5a984e59ace4d08f
Vvamp <vvampus@gmail.com>
2020-05-28 10:43:19 +0200

    cleaned up code

--- main.py
+++ main.py
@@ -1,33 +1,51 @@
 from tkinter import *
 from math import sin
-dataFile = open("data3.txt",  'r')
 
 def clamp(x): 
   return max(0, min(x, 255))
 
-WIDTH, HEIGHT = 1920, 1080
 
-window = Tk()
-canvas = Canvas(window, width=WIDTH, height=HEIGHT, bg="#000000")
-canvas.pack()
-img = PhotoImage(width=WIDTH, height=HEIGHT)
-canvas.create_image((WIDTH/2, HEIGHT/2), image=img, state="normal")
+def loadData(imagePath, WIDTH, HEIGHT):
+    print("> Opening image data...")
+    dataFile = open(imagePath, "r")
+    print("> Reading data...", end="")
+    data = dataFile.read()
+    print(" success!")
 
-data = dataFile.read()
-rgbdata = []
-for rgbval in data.split(' '):
-    if(len(rgbval) <= 1):
-        continue
-    rgbval = rgbval.replace('(', '')
-    rgbval = rgbval.replace(')', '')
-    red = int(rgbval.split(',')[0])
-    green = int(rgbval.split(',')[1])
-    blue = int(rgbval.split(',')[2])
-    rgbdata.append("#{0:02x}{1:02x}{2:02x}".format(clamp(red), clamp(green), clamp(blue)))
+    print("> Parsing data...", end="")
+    rgbdata = []
+    for rgbval in data.split(' '):
+        if(len(rgbval) <= 1):
+            continue
+        rgbval = rgbval.replace('(', '')
+        rgbval = rgbval.replace(')', '')
+        red = int(rgbval.split(',')[0])
+        green = int(rgbval.split(',')[1])
+        blue = int(rgbval.split(',')[2])
+        rgbdata.append("#{0:02x}{1:02x}{2:02x}".format(clamp(red), clamp(green), clamp(blue)))
+    print(" success!")
+
+    print ("> Rendering Image...", end="")
+    window = Tk()
+    canvas = Canvas(window, width=1920, height=1080, bg="#000000")
+    canvas.pack()
+    img = PhotoImage(width=WIDTH, height=HEIGHT)
+    canvas.create_image((WIDTH/2, HEIGHT/2), image=img, state="normal")
+
+    for x in range(0, 120):
+        for y in range(0, 160):
+            img.put(rgbdata[x+y], (x,y))
+    print(" success!")
+
+    mainloop()
 
 
-for x in range(0, 120):
-    for y in range(0, 160):
-        img.put(rgbdata[x+y], (x,y))
+def init():
+    imagePath = input("Please enter the path to the image data > ")
+    width = int(input("Image width > "))
+    height = int(input("Image height > "))
 
-mainloop()
+    loadData(imagePath, width, height)
+
+
+init()
